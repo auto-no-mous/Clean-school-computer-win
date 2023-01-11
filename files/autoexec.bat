47813f4947feca2d008a8d7cdb836779ff3134ed
Aleksandr Kiselyov <67557657+auto-no-mous@users.noreply.github.com>
2023-01-11 23:34:44 +0300

    Add files via upload

--- files/autoexec.bat
+++ files/autoexec.bat
@@ -12,8 +12,15 @@ reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E} /ve /f 
 echo *** Default names restored ***
 
+echo *** Set wallpaper to "stretch" setting ***
+REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f
+REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+echo *** Wallpaper settings restored ***
+
+::remove this block if you have Windows 8 or newer
 echo *** Refresh desktop... ***
 setlocal EnableExtensions DisableDelayedExpansion
 ie4uinit.exe -show
+
 endlocal
 echo *** All done. ***
--- files/disable_personalization.bat
+++ files/disable_personalization.bat
@@ -13,13 +13,13 @@ REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Sy
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Windows\Sidebar" /v TurnOffSidebar /t REG_DWORD /d 1 /f
 REG ADD "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Explorer" /v NoUninstallFromStart /t REG_DWORD /d 1 /f
 
-::настройки, влияющие только на windows 8 и новее
+::настройки, влияющие только на windows 8 и новее (все пользователи)
 ::полный список параметров, которые можно скрыть, смотрите здесь:
 ::https://learn.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app#ms-settings-uri-scheme-reference
 
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v SettingsPageVisibility /t REG_SZ /d hide:personalization-background;personalization-start-places;lockscreen;themes;personalization-start;personalization-background;display;personalization-colors;colors;nightlight;powersleep;tabletmode;remotedesktop;autoplay;network-wifi;colors;taskbar;signinoptions;otherusers;regionlanguage;speech;gaming-gamebar;gaming-broadcasting;gaming-gamemode;gaming-trueplay;gaming-xboxnetworking;windowsdefender;screenrotation;personalization-glance;fonts;easeofaccess-mouse;easeofaccess-mousepointer;easeofaccess-highcontrast;easeofaccess-display;easeofaccess-colorfilter;easeofaccess-colorfilter-adaptivecolorlink;easeofaccess-colorfilter-bluelightlink;easeofaccess-keyboard;easeofaccess-highcontrast;easeofaccess-visualeffects;defaultapps;startupapps;appsfeatures;appsfeatures-app /f
+REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v SettingsPageVisibility /t REG_SZ /d hide:personalization-background;personalization-start-places;lockscreen;themes;personalization-start;personalization-background;display;personalization-colors;colors;nightlight;powersleep;tabletmode;remotedesktop;autoplay;network-wifi;colors;taskbar;signinoptions;otherusers;regionlanguage;speech;gaming-gamebar;gaming-broadcasting;gaming-gamemode;gaming-trueplay;gaming-xboxnetworking;windowsdefender;screenrotation;personalization-glance;fonts;easeofaccess-mouse;easeofaccess-mousepointer;easeofaccess-highcontrast;easeofaccess-display;easeofaccess-colorfilter;easeofaccess-colorfilter-adaptivecolorlink;easeofaccess-colorfilter-bluelightlink;easeofaccess-keyboard;easeofaccess-highcontrast;easeofaccess-visualeffects;defaultapps;startupapps;appsfeatures;appsfeatures-app /f
 
-::настройки влияющие на все учетные записи
+::настройки влияющие Windows 7 и новее
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoChangingWallPaper /t REG_DWORD /d 1 /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoAddingComponents /t REG_DWORD /d 1 /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v NoComponents /t REG_DWORD /d 1 /f
